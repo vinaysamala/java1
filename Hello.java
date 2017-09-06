7effc77bfb0e9ac1d49380d569aff333453908e6
vinay <samalavinay@gmail.com>
2017-09-06 16:55:52 -0400

    asd

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Hello 
+{
+	public static void main(String args[])
+	{
+		int x,y,z;
+		System.out.println("enter ");
+		Scanner in = new Scanner (System.in);
+		x = in.nextInt();
+		y = in.nextInt();
+		z= x+y;
+		System.out.println("Sum = "+z);
+	}
+}
+
